697312b5337766348bfecc4f314f094a1a3f8530
Uttej Kumar <uttejkumarponugoti@gmail.com>
2021-02-04 17:26:35 +0530

    new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,12 @@ class ArticlesController < ApplicationController
     end
 
     def new
+
+    end
+
+    def create
+        @article=Article.new(params.require(:article).permit(:cr_no,:sec_law,:acc_name))
+        @article.save
+        redirect_to @article
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord 
-    
+    validates :cr_no, presence: true, length: {minimum:2, maximum:10}
+    validates :sec_law,presence:true, length: {minimum:2,maximun: 50}
+    validates :acc_name,presence:true, length: {minimum:2,maximun: 50}
 end
   
